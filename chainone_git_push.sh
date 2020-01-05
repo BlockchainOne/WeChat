88e4da8aff5153e3edfad42a855cd2aa599c9089
Robin Wen <blockxyz@gmail.com>
2020-01-05 19:37:52 +0800

    [Blockchain] WeChat Group Summary, 1th week of 2020

--- chainone_git_push.sh
+++ chainone_git_push.sh
@@ -4,4 +4,4 @@
 WEEK=$1
 
 # Auto push
-git add -A . && git commit -S -m "[Blockchain] WeChat Group Summary, ${WEEK}th week of 2019" && git push origin master
+git add -A . && git commit -S -m "[Blockchain] WeChat Group Summary, ${WEEK}th week of 2020" && git push origin master
